80583e3967779b64f62d262bbd8b474940a15dea
Alisson Oliveira <alisson_oliveira010203@hotmail.com>
2019-10-18 14:50:52 -0300

    Adicionado questão 1

---
+++ listas/lista-de-exercicio-02/Questão -01.py
@@ -0,0 +1,7 @@
+notas = input('Digite suas notas separadas por espaço : ').split()
+soma= 0
+media= 0
+for i in notas:
+	i = int(i)
+	soma += i
+print('Sua media é : {}',format(soma/4))
---
+++ listas/lista-de-exercicio-02/Questão -02.py
@@ -0,0 +1,3 @@
+numero1 = int(input())
+numero2 = int(input())
+print('A soma é :'.format(numero1 + numero2))
