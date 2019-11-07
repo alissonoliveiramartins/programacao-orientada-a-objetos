4373464d4c973a62cb83e5a001d03b95fafc5ff5
Alisson Oliveira <alisson_oliveira010203@hotmail.com>
2019-11-07 11:07:40 -0300

    Atualizando programa para envio e conclusão da atividade

--- listas/lista-de-exercicio-04/exercicio-04.py
+++ listas/lista-de-exercicio-04/exercicio-04.py
@@ -1,42 +1,48 @@
 with open("amazon.csv") as arquivo:
 	
-	ano = 0
-	cidade= ' '
-	mes = ' '
-	casos= 0
-	data = 0
 	item_a = 0
 	item_b = 0
 	item_c = 0
 	item_d = 0
+	todos_estados = set()
 
 	for linha in arquivo:
 		linha = linha.strip(' ')
-		ano, cidade, mes, casos, data = linha.split(',')
+		ano, estado, mes, casos, data = linha.split(',')
 		casos= casos.replace(".", "")
+		estado= estado.replace('"','')
 		#Item A
-		if '"Acre"' == cidade and ano == '2015':
+		if 'Acre' == estado and ano == '2015':
 			item_a += int(casos)
 
 		#Item B
-		elif '"Ceara"' == cidade and ano == '2014':
+		elif 'Ceara' == estado and ano == '2014':
 			item_b += int(casos)
 
 		#Item C
-		elif '"Amazonas"' == cidade:
+		elif 'Amazonas' == estado:
 			item_c += int(casos)
 
 		#Item D
 		if ano == '"year"':
 			continue
 
-		elif int(ano) >= 2010 and cidade == '"Mato Grosso"':
+		elif int(ano) >= 2010 and estado == 'Mato Grosso':
 			item_d += int(casos)
 
+		#PERGUNTA ADICIONAL	
+		todos_estados.add(estado)
+		
+        #Respostas 
 	print('Resposta item A : {}'.format(item_a))
 	print('Resposta item B : {}'.format(item_b))
 	print('Resposta item C : {}'.format(item_c))
 	print('Resposta item D : {}'.format(item_d))
+	print('\nPERGUNTA ADICIONAL:')
 
-if arquivo.closed:
-	print('ARQUIVO FECHADO')
+        #Pergunta Adicional - Quantos estados participaram da pesquisa? E quais foram eles ?
+	
+	print('Foram {} Estados.\nSão eles : {}.'.format(len(todos_estados),', '.join(map(str, todos_estados))))
+	
+#if arquivo.closed:
+#	print('ARQUIVO FECHADO')
