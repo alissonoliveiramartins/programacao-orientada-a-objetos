990e9a5976b0bb2c3f84d23d9c8af358607c6d89
alissonoliveiramartins <56736959+alissonoliveiramartins@users.noreply.github.com>
2019-11-29 15:03:55 -0300

    Adicionado prova

---
+++ prova/prova_01.py
@@ -0,0 +1,29 @@
+with open('usuarios.txt' ) as prova:
+    with open("relatorio.txt", "w") as saida:
+
+        def conversao(numero_byts):
+            return (int(numero_byts) /1024 / 1024)
+     
+       
+        lista_dados = []
+        lista_nomes =[]
+        lista_percentual= []
+
+        soma = 0
+        numeracao = 1
+       
+        for x in prova:
+            x = x.strip()
+            nomes ,dados = x.split()
+            lista_dados.append(dados)
+            lista_nomes.append(nomes)
+            percentual = (int(dados)*100 )
+            lista_percentual.append(percentual)
+            soma += int(dados)
+        print('ACME Inc.           Uso do espeço em disco pelos usuários\n{}\nNr.        Usuario        Espaço Utilizado        % do uso\n'.format('-'*60), file=saida)
+
+        
+        for indice in range(0,len(lista_nomes)):
+            print('{}      {}       {:.2f}MB         {:.2f} %'.format(indice+1,lista_nomes[indice].center(15),conversao(lista_dados[indice]),int(lista_percentual[indice])/soma), file=saida)        
+            
+ 
