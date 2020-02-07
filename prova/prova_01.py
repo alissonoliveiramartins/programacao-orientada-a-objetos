0218547028e3fbae270bea04cbbe9e9654e5f0b2
alissonoliveiramartins <56736959+alissonoliveiramartins@users.noreply.github.com>
2020-02-07 16:20:03 -0300

    Atualizando a prova

--- prova/prova_01.py
+++ prova/prova_01.py
@@ -20,10 +20,14 @@ with open('usuarios.txt' ) as prova:
             percentual = (int(dados)*100 )
             lista_percentual.append(percentual)
             soma += int(dados)
+           
         print('ACME Inc.           Uso do espeço em disco pelos usuários\n{}\nNr.        Usuario        Espaço Utilizado        % do uso\n'.format('-'*60), file=saida)
 
         
         for indice in range(0,len(lista_nomes)):
-            print('{}      {}       {:.2f}MB         {:.2f} %'.format(indice+1,lista_nomes[indice].center(15),conversao(lista_dados[indice]),int(lista_percentual[indice])/soma), file=saida)        
-            
+            print('{:<3}      {:<10}       {:>8.2f}MB         {:>6.2f} %'.format(indice+1,lista_nomes[indice].center(15),conversao(lista_dados[indice]),int(lista_percentual[indice])/soma), file=saida)        
+        soma = (soma/1024/1024)
+        print(' ', file=saida)
+        print('Espaço total ocupado: {:.2f} MB'.format(soma), file=saida)  
+        print('Espaço médio ocupado: {:.2f} MB'.format(soma/len(lista_nomes)), file=saida)
  
