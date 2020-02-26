48cd54c11ebfa64b4a9555e945020fd982d58fbe
Alisson Oliveira <alisson_oliveira010203@hotmail.com>
2020-02-26 17:03:03 -0300

    Corrigindo espaçamento e outros erros

--- Atividade_N2/criando_uma_conta.py
+++ Atividade_N2/criando_uma_conta.py
@@ -1,16 +1,16 @@
-def	criar_conta(numero,	titular,	saldo,	limite):
+def cria_conta(numero, titular, saldo, limite):
     """Função responsavel por criar a conta"""
     conta={"numero":numero,"titular":titular,"saldo":saldo,"limite":limite}
-    return	conta
-def	deposita(conta,	valor):
-	conta['saldo'] += valor
-	"""Funçaõ adiciona o valor a conta"""
+    return conta
 
-def	saca(conta,	valor):
+def deposita(conta, valor):
+    """Funçaõ adiciona o valor a conta"""
+    conta['saldo'] += valor
+    
+def saca(conta,	valor):
     """Função subtrai o valor da conta"""
-    conta['saldo']	-=	valor
+    conta['saldo'] -= valor
    
-	
-def	extrato(conta):
-    """Esta função imrimi o extrado da conta"""
+def extrato(conta):
+    """Esta função imprimi o extrato da conta"""
     print("numero: {} \nsaldo: {}".format(conta['numero'], conta['saldo']))
